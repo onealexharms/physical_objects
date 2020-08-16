5ebbb1ab7ff153db5a3f0431e0200dcc5353cdaa
Alex Harms <onealexharms@gmail.com>
2020-08-16 19:30:19 -0400

    Start refactoring to use layouts

--- breadboard-fun/led-module/led-module.clj
+++ breadboard-fun/led-module/led-module.clj
@@ -1,9 +1,13 @@
-(require '[dali.io :as io])
+(require
+  '[dali.io :as io]
+  '[dali.layout.distribute]
+  '[dali.layout.stack])
 
 (def ^:private grid-spacing 2.54)
 (def ^:private group-spacing (* 3 grid-spacing))
 (def ^:private row-spacing (* 1.5 grid-spacing))
 (def ^:private column-spacing grid-spacing)
+(def ^:private margin 25)
 
 (defn- hole [x y]
   [:circle {:cx x 
@@ -18,25 +22,33 @@
   [:g (hole (* grid-spacing 3) y)
       (hole (* grid-spacing 4) y)])
 
-(defn- resistor [y]
-  [:g (hole (* grid-spacing 5) y)
-      (hole (* grid-spacing 8) y)])
+(def ^:private resistor
+  [:g (hole (* grid-spacing 5) 0)
+      (hole (* grid-spacing 8) 0)])
 
-(defn- led-entourage [y]
-  [:g (socket y)
-      (led y)
-      (resistor y)])
+(def ^:private led-entourage
+  [:g (socket 0)
+      (led 0)
+      resistor])
 
 (defn- group-of-four [group-number]
-  [:g 
-   (led-entourage (* grid-spacing (inc (* group-number group-spacing))))
-   (led-entourage (* grid-spacing (inc (+ 1 (* group-number group-spacing)))))
-   (led-entourage (* grid-spacing (inc (+ 2 (* group-number group-spacing)))))
-   (led-entourage (* grid-spacing (inc (+ 3 (* group-number group-spacing)))))])
+  [:dali/distribute {:direction :down,
+                     :anchor :center,
+                     :gap row-spacing}
+   led-entourage
+   led-entourage
+   led-entourage
+   led-entourage])
 
 (def led-module
   [:dali/page
-   (group-of-four 0)
-   (group-of-four 1)])
+   [:dali/distribute {:direction :down,
+                      :anchor :center,
+                      :position [margin margin]
+                      :gap group-spacing}
+     (group-of-four 0)
+     (group-of-four 1)
+     (group-of-four 2)
+     (group-of-four 3)]])
 
 (io/render-svg led-module "led-module.svg")
