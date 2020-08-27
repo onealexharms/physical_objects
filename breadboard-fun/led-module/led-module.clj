76b05e93b4f9b43a405969b9d12bcaf993a3ae0e
Alex Harms <onealexharms@gmail.com>
2020-08-27 16:47:50 -0400

    Use opencircuit

--- breadboard-fun/led-module/led-module.clj
+++ breadboard-fun/led-module/led-module.clj
@@ -1,14 +1,15 @@
 (require
   '[dali.io :as io]
-  '[dali.layout.distribute])
+  '[dali.layout.distribute]
+  '[opencircuit.core :refer :all])
 
-(def ^:private grid-spacing 2.54)
-(def ^:private group-spacing (* 5 grid-spacing))
-(def ^:private row-spacing (* 1.5 grid-spacing))
-(def ^:private column-spacing grid-spacing)
-(def ^:private margin 25)
-(def ^:private board-width 110)
-(def ^:private board-height 110)
+(define grid-spacing 2.54)
+(define group-spacing (* 5 grid-spacing))
+(define row-spacing (* 1.5 grid-spacing))
+(define column-spacing grid-spacing)
+(define margin 25)
+(define board-width 110)
+(define board-height 110)
 
 (defn- hole [x y]
   [:circle {:cx x 
@@ -17,23 +18,23 @@
             :fill :black
             :stroke :none}])
 
-(def ^:private socket
+(define socket
   (hole (* grid-spacing 1) 0))
 
-(def ^:private led
+(define led
   [:g (hole (* grid-spacing 3) 0)
       (hole (* grid-spacing 4) 0)])
 
-(def ^:private resistor
+(define resistor
   [:g (hole (* grid-spacing 6) 0)
       (hole (* grid-spacing 9) 0)])
 
-(def ^:private led-entourage
+(define led-entourage
   [:g socket
       led
       resistor])
 
-(def ^:private group-of-four
+(define group-of-four
   [:dali/distribute {:direction :down,
                      :anchor :center,
                      :step grid-spacing}
@@ -42,7 +43,7 @@
    led-entourage
    led-entourage])
 
-(def led-module
+(define led-module
    [:dali/distribute {:direction :down,
                       :anchor :bottom,
                       :position [margin margin],
@@ -53,14 +54,14 @@
      group-of-four
      group-of-four])
 
-(def cutout
+(define cutout
   [:rect {:dali/z-index -99,
           :stroke :black,
           :fill :white}
    [0 0]
    [board-width board-height]])
 
-(def board
+(define board
   [:dali/page
    cutout
    led-module])
