764f1914859bf915d8cac247b07498bc5cb6248e
Alex Harms <onealexharms@gmail.com>
2020-09-13 14:57:58 -0400

    Use open-circuitry board definiton

--- breadboard-fun/led-module/led-module.clj
+++ breadboard-fun/led-module/led-module.clj
@@ -64,9 +64,8 @@
    [board-width board-height]])
 
 (board "led-module"
-  [:dali/page
-   {:width (str document-width "mm")
-    :height (str document-height "mm")
-    :view-box (str "0 0 " document-width " " document-height)}
-   (position cutout margin margin)
-   (position led-module starting-x starting-y)])
+  [:open-circuitry/board
+   {:width document-width
+    :height document-height}])
+   ;(position cutout margin margin)
+   ;(position led-module starting-x starting-y)])
